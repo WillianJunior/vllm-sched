e011581e52fe393bdd953b69764c6564fe018fdc
Willian <guns945@gmail.com>
2025-09-02 19:01:02 -0300

    Created and tested submission script.

---
+++ submit-dataset.py
@@ -0,0 +1,138 @@
+from time import time, sleep
+import argparse
+from datasets import load_dataset
+from math import ceil
+from tqdm import tqdm
+import requests
+import concurrent.futures
+
+
+url_base = "http://127.0.0.1:8000"
+
+def prep_parser():
+    parser = argparse.ArgumentParser(
+        description="Submitter for sequences of LLM datasets.")
+
+    parser.add_argument(
+        '--dataset', 
+        type=str, 
+        help='Dataset to execute. Example: tatsu-lab/alpaca.', 
+        required=True,
+    )
+
+    parser.add_argument(
+        '--interval', 
+        type=int, 
+        help='Interval in seconds between batch submissions (default=10).', 
+        default=10,
+    )
+
+    parser.add_argument(
+        '--bs', 
+        type=int, 
+        help='Batch size of a submission (default=250).', 
+        default=250,
+    )
+
+    return parser
+
+def get_requests(dataset_name):
+    ds = load_dataset(dataset_name)
+    requests = []
+    if dataset_name == 'tatsu-lab/alpaca':
+        for d in tqdm(ds['train'], desc="Preping requests from dataset"):
+            instruction = d['instruction']
+            input_txt = d['input']
+            r = """Below is an instruction that describes a task,\
+                paired with an input that provides further context.\
+                Write a response that appropriately completes the request.\
+                \n\n### Instruction:\n"""
+            r += instruction + "\n\n"
+            if len(input_txt) > 0:
+                r += "### Input:\n"
+                r += input_txt
+            r += "\n\n### Response:\n"
+            requests.append(r)
+
+    assert len(requests) > 0, f"dataset name not recognized: {dataset_name}"
+
+    return requests
+
+# Retry logic to wait until the server is up
+def wait_for_server(timeout=60, retry_interval=2):
+    start_time = time()
+    while time() - start_time < timeout:
+        try:
+            response = requests.get(url_base + "/health")
+            if response.status_code < 500:
+                print("Server is up!")
+                return True
+        except requests.exceptions.ConnectionError:
+            pass  # Server not up yet
+        print(f"Waiting for server at {url}...")
+        sleep(retry_interval)
+    print("Timeout waiting for server.")
+    return False
+
+
+def submit_request(prompts_batch):
+    url_request = url_base + "/v1/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer EMPTY"  # vLLM requires this even if it's not used
+    }
+
+    payload = {
+        "model": "/snfs1/llm-models/llama-3.2-3B-Instruct",
+        "prompt": prompts_batch,
+        "max_tokens": 2048,
+        "temperature": 0.0
+    }
+
+    def send_req():
+        return requests.post(url_request, headers=headers, json=payload)
+
+    executor = concurrent.futures.ThreadPoolExecutor()
+    future = executor.submit(send_req)
+    return executor, future
+
+def wait_all_requests(futures):
+    results = []
+    for _executor, future in tqdm(futures, desc="Waiting for returning reqs"):
+        result = future.result()
+        results.append(result)
+    return results
+
+def write_stats(results):
+    for r in results:
+        assert r.status_code == 200
+        #print(r.json()) nothing yet...
+        #return
+
+def main():
+    parser = prep_parser()
+    args = parser.parse_args()
+    
+    requests = get_requests(args.dataset)
+    requests = requests[:5]
+    num_batches = ceil(len(requests) / args.bs)
+    futures = []
+
+    assert wait_for_server(), "Couldn't connect to vLLM server."
+
+    # Submit each batch of requests
+    for b in tqdm(range(num_batches), total=num_batches, desc="Sending batches"):
+        init = b * args.bs
+        end = init + args.bs + 1
+        batch = requests[init:end]
+        futures.append(submit_request(batch))
+        if b < num_batches - 1:
+            # don't sleep the last interval
+            sleep(args.interval)
+
+    results = wait_all_requests(futures)
+    write_stats(results)
+
+
+if __name__ == '__main__':
+    main()
