af262633e758b66924c0e98888d1f018984491dd
WillianJunior <guns945@gmail.com>
2025-09-24 13:50:16 -0300

    .

--- schedulers/fcfs.py
+++ schedulers/fcfs.py
@@ -1,8 +1,8 @@
 from vllm.v1.core.sched.interface import SchedulerInterface
-from vllm.v1.core.sched.output import CachedRequestData, NewRequestData, SchedulerOutput
-from vllm.v1.outputs import DraftTokenIds, KVConnectorOutput, ModelRunnerOutput
-from vllm.v1.engine import EngineCoreEventType, EngineCoreOutput, EngineCoreOutputs
-
+from vllm.v1.core.sched.output import SchedulerOutput
+from vllm.v1.outputs import ModelRunnerOutput
+from vllm.v1.engine import EngineCoreOutputs
+from vllm.v1.request import Request, RequestStatus
 
 class MyFCFSSched(SchedulerInterface):
     """
@@ -26,6 +26,7 @@ class MyFCFSSched(SchedulerInterface):
         print(f"\t mm_registry: {mm_registry}")
         print(f"\t include_finished_set: {include_finished_set}")
         print(f"\t log_stats: {log_stats}")
+
 
     def schedule(self) -> SchedulerOutput:
         """Schedule the requests to process in this scheduling step.
@@ -52,6 +53,7 @@ class MyFCFSSched(SchedulerInterface):
         """
         raise NotImplementedError
 
+
     def update_from_output(
         self,
         scheduler_output: SchedulerOutput,
@@ -71,12 +73,55 @@ class MyFCFSSched(SchedulerInterface):
         """
         raise NotImplementedError
 
-    def update_draft_token_ids(
-        self,
-        draft_token_ids: DraftTokenIds,
-    ) -> None:
-        """Update the draft token ids for the scheduled requests."""
+
+    def add_request(self, request: Request) -> None:
+        """Add a new request to the scheduler's internal queue.
+        
+        Args:
+            request: The new request being added.
+        """
         raise NotImplementedError
+
+
+    def finish_requests(
+        self,
+        request_ids: Union[str, Iterable[str]],
+        finished_status: RequestStatus,
+    ) -> None:
+        """Finish the requests in the scheduler's internal queue. If the request
+        is not in the queue, this method will do nothing.
+
+        This method is called in two cases:
+        1. When the request is aborted by the client.
+        2. When the frontend process detects a stop string of the request after
+           de-tokenizing its generated tokens.
+           
+        Args:
+            request_ids: A single or a list of request IDs.
+            finished_status: The finished status of the given requests.
+        """
+        raise NotImplementedError
+
+
+    def get_num_unfinished_requests(self) -> int:
+        """Number of unfinished requests in the scheduler's internal queue."""
+        raise NotImplementedError
+
+
+    def has_finished_requests(self) -> bool:
+        """Returns True if there are finished requests that need to be cleared.
+        NOTE: This is different from `not self.has_unfinished_requests()`.
+
+        The scheduler maintains an internal list of the requests finished in the
+        previous step. This list is returned from the next call to schedule(),
+        to be sent to the model runner in the next step to clear cached states
+        for these finished requests.
+
+        This method checks if this internal list of finished requests is
+        non-empty. This information is useful for DP attention.
+        """
+        raise NotImplementedError
+
 
     def reset_prefix_cache(self) -> bool:
         """Reset the prefix cache for KV cache.
@@ -85,16 +130,19 @@ class MyFCFSSched(SchedulerInterface):
         """
         raise NotImplementedError
 
+
     def get_request_counts(self) -> tuple[int, int]:
         """Returns (num_running_reqs, num_waiting_reqs)."""
         raise NotImplementedError
 
-    def make_stats(self) -> Optional[SchedulerStats]:
+
+    def make_stats(self) -> Optional["SchedulerStats"]:
         """Make a SchedulerStats object for logging.
 
         The SchedulerStats object is created for every scheduling step.
         """
         raise NotImplementedError
+
 
     def shutdown(self) -> None:
         """Shutdown the scheduler."""
