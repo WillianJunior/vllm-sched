7617f3b20c860823b79ae52ab63f6ee8e211e36d
Willian Junior <willianjunior@dcc.ufmg.br>
2026-02-10 11:55:27 -0300

    rework of dirs

---
+++ studies/3-goodput-many-scheds/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# all succede or fail
+set -e
+
+export BASE_PATH=$(git rev-parse --show-toplevel)/envs
+export VLLM_ENV_NAME=vllm-0.11.1
+export PYTHON_VERSION=3.11
+
+# create and activate conda env
+conda create -y -p "$BASE_PATH/$VLLM_ENV_NAME" python="$PYTHON_VERSION"
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate "$BASE_PATH/$VLLM_ENV_NAME"
+
+# install uv and vllm
+pip3 install --upgrade uv
+uv pip install vllm==0.10.1
