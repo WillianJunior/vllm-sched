c505055fd1e77b1c842617fce838a1363bb22455
Willian <willianjunior@dcc.ufmg.br>
2026-03-02 12:22:08 -0300

    install

--- studies/3-goodput-many-scheds/0-install.sh
+++ studies/3-goodput-many-scheds/0-install.sh
@@ -7,8 +7,8 @@ set -e -x
 
 export BASE_PATH=$(git rev-parse --show-toplevel)/envs
 #export VLLM_ENV_NAME=vllm-0.10.1
-#export VLLM_ENV_NAME=vllm-0.9.2
-export VLLM_ENV_NAME=vllm-0.16.0
+export VLLM_ENV_NAME=vllm-0.9.2
+#export VLLM_ENV_NAME=vllm-0.16.0
 export PYTHON_VERSION=3.11
 
 # create and activate conda env
@@ -21,8 +21,12 @@ pip3 install --upgrade uv
 #uv pip install "transformers==4.33.0" "tokenizers==0.13.3"
 #uv pip install vllm==0.10.2
 
+# for blackwell gpus (sm_120)
+# still not working...
+uvpip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
+
 #uv pip install "vllm==0.10.1" "transformers==4.57.6"
-#uv pip install "vllm==0.9.2" #"transformers==4.57.6"
-uv pip install "vllm==0.16.0"
+uv pip install "vllm==0.9.2" "transformers==4.53.0"
+#uv pip install "vllm==0.16.0"
 
 uv pip install pandas datasets scikit-learn joblib
