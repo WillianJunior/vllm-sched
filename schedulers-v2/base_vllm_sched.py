5af9d079962d22be78bbcbce55e96ec58e371f3d
WillianJunior <guns945@gmail.com>
2026-03-25 16:41:35 -0300

    Testing bare base scheduler for v0.16.0

---
+++ schedulers-v2/base_vllm_sched.py
@@ -0,0 +1,342 @@
+import time
+# from collections import defaultdict, deque
+# from collections.abc import Iterable
+# from dataclasses import replace
+# from typing import Any
+
+# import numpy as np
+
+from vllm.config import VllmConfig
+# from vllm.distributed.ec_transfer.ec_connector.base import ECConnectorMetadata
+# from vllm.distributed.kv_transfer.kv_connector.v1.base import KVConnectorMetadata
+from vllm.logger import init_logger
+from vllm.multimodal import MULTIMODAL_REGISTRY, MultiModalRegistry
+from vllm.v1.core.kv_cache_manager import KVCacheBlocks
+from vllm.v1.core.sched.output import NewRequestData, SchedulerOutput
+from vllm.v1.core.sched.request_queue import create_request_queue
+from vllm.v1.engine import EngineCoreEventType
+from vllm.v1.kv_cache_interface import KVCacheConfig
+from vllm.v1.request import Request, RequestStatus, StreamingUpdate
+from vllm.v1.core.sched import Scheduler
+from vllm.v1.structured_output import StructuredOutputManager
+from vllm.v1.utils import record_function_or_nullcontext
+
+logger = init_logger(__name__)
+
+class BaseScheduler(Scheduler):
+    def __init__(
+        self,
+        vllm_config: VllmConfig,
+        kv_cache_config: KVCacheConfig,
+        structured_output_manager: StructuredOutputManager,
+        block_size: int,
+        mm_registry: MultiModalRegistry = MULTIMODAL_REGISTRY,
+        include_finished_set: bool = False,
+        log_stats: bool = False,
+    ) -> None:
+        # Base Scheduler init...
+        super().__init__(
+            vllm_config,
+            kv_cache_config,
+            structured_output_manager,
+            block_size,
+            mm_registry,
+            include_finished_set,
+            log_stats,
+        )
+
+    def schedule(self) -> SchedulerOutput:
+        # NOTE(will) just basic decoder-only scheduling
+        # no spec decoding
+        # no encoders
+        # no mamba
+        # no LoRA
+        # no remote KVS
+        # no structured output wif FSM
+        # always with chunked prefill
+        # no P/D Disaggregation
+
+        scheduled_new_reqs: list[Request] = []
+        scheduled_resumed_reqs: list[Request] = []
+        scheduled_running_reqs: list[Request] = []
+        preempted_reqs: list[Request] = []
+
+        req_to_new_blocks: dict[str, KVCacheBlocks] = {}
+        num_scheduled_tokens: dict[str, int] = {}
+        token_budget = self.max_num_scheduled_tokens
+        # Encoder-related.
+        scheduled_encoder_inputs: dict[str, list[int]] = {}
+        encoder_compute_budget = self.max_num_encoder_input_tokens
+        # Spec decode-related.
+        scheduled_spec_decode_tokens: dict[str, list[int]] = {}
+
+        # For logging.
+        scheduled_timestamp = time.monotonic()
+
+        # First, schedule the RUNNING requests.
+        req_index = 0
+        while req_index < len(self.running) and token_budget > 0:
+            request = self.running[req_index]            
+
+            num_new_tokens = 1
+            num_new_tokens = min(num_new_tokens, token_budget)
+
+            if num_new_tokens == 0:
+                # The request cannot be scheduled because one of the following
+                # reasons:
+                # 1. No new tokens to schedule. This may happen when
+                #    (1) PP>1 and we have already scheduled all prompt tokens
+                #    but they are not finished yet.
+                #    (2) Async scheduling and the request has reached to either
+                #    its max_total_tokens or max_model_len.
+                # NOTE(woosuk): Here, by doing `continue` instead of `break`,
+                # we do not strictly follow the FCFS scheduling policy and
+                # allow the lower-priority requests to be scheduled.
+                req_index += 1
+                continue
+
+            # Schedule newly needed KV blocks for the request.
+            with record_function_or_nullcontext("schedule: allocate_slots"):
+                while True:
+                    new_blocks = self.kv_cache_manager.allocate_slots(
+                        request,
+                        num_new_tokens,
+                        num_lookahead_tokens=self.num_lookahead_tokens,
+                    )
+
+                    if new_blocks is not None:
+                        # The request can be scheduled.
+                        break
+
+                    preempted_req = self.running.pop()
+
+                    self._preempt_request(preempted_req, scheduled_timestamp)
+                    preempted_reqs.append(preempted_req)
+                    if preempted_req == request:
+                        # No more request to preempt. Cannot schedule this request.
+                        break
+
+            if new_blocks is None:
+                # Cannot schedule this request.
+                break
+
+            # Schedule the request.
+            scheduled_running_reqs.append(request)
+            request_id = request.request_id
+            req_to_new_blocks[request_id] = new_blocks
+            num_scheduled_tokens[request_id] = num_new_tokens
+            token_budget -= num_new_tokens
+            req_index += 1
+
+        # Use a temporary RequestQueue to collect requests that need to be
+        # skipped and put back at the head of the waiting queue later
+        skipped_waiting_requests = create_request_queue(self.policy)
+
+        # Next, schedule the WAITING requests.
+        if not preempted_reqs:
+            while self.waiting and token_budget > 0:
+                if len(self.running) == self.max_num_running_reqs:
+                    break
+
+                request = self.waiting.peek_request()
+                request_id = request.request_id
+
+                # NOTE(Will): what is streaming???
+                # # Streaming: skip request if still waiting for next streaming req.
+                # if request.status == RequestStatus.WAITING_FOR_STREAMING_REQ:
+                #     assert not request.streaming_queue
+                #     self.waiting.pop_request()
+                #     skipped_waiting_requests.prepend_request(request)
+                #     continue
+
+                num_external_computed_tokens = 0
+                load_kv_async = False
+                # connector_prefix_cache_queries, connector_prefix_cache_hits = 0, 0
+
+                # Get already-cached tokens.
+                if request.num_computed_tokens == 0:
+                    # Get locally-cached tokens.
+                    new_computed_blocks, num_new_local_computed_tokens = (
+                        self.kv_cache_manager.get_computed_blocks(request)
+                    )
+
+                    # Total computed tokens (local + external).
+                    num_computed_tokens = num_new_local_computed_tokens
+                else:
+                    # KVTransfer: WAITING reqs have num_computed_tokens > 0
+                    # after async KV recvs are completed.
+                    new_computed_blocks = self.kv_cache_manager.empty_kv_cache_blocks
+                    num_new_local_computed_tokens = 0
+                    num_computed_tokens = request.num_computed_tokens
+
+                encoder_inputs_to_schedule = None
+                external_load_encoder_input = []
+                new_encoder_compute_budget = encoder_compute_budget
+
+                # Number of tokens to be scheduled.
+                # We use `request.num_tokens` instead of
+                # `request.num_prompt_tokens` to consider the resumed
+                # requests, which have output tokens.
+                num_new_tokens = request.num_tokens - num_computed_tokens
+
+                num_new_tokens = min(num_new_tokens, token_budget)
+                assert num_new_tokens > 0
+
+                num_encoder_tokens = 0
+
+                new_blocks = self.kv_cache_manager.allocate_slots(
+                    request,
+                    num_new_tokens,
+                    num_new_computed_tokens=num_new_local_computed_tokens,
+                    new_computed_blocks=new_computed_blocks,
+                    num_lookahead_tokens=effective_lookahead_tokens,
+                    num_external_computed_tokens=num_external_computed_tokens,
+                    delay_cache_blocks=load_kv_async,
+                    num_encoder_tokens=num_encoder_tokens,
+                )
+
+                if new_blocks is None:
+                    # The request cannot be scheduled.
+                    break
+
+                # # KVTransfer: the connector uses this info to determine
+                # # if a load is needed. Note that
+                # # This information is used to determine if a load is
+                # # needed for this request.
+                # if self.connector is not None:
+                #     self.connector.update_state_after_alloc(
+                #         request,
+                #         self.kv_cache_manager.get_blocks(request_id),
+                #         num_external_computed_tokens,
+                #     )
+                #     if (
+                #         self.connector_prefix_cache_stats is not None
+                #         and connector_prefix_cache_queries != 0
+                #     ):
+                #         self.connector_prefix_cache_stats.record(
+                #             num_tokens=connector_prefix_cache_queries,
+                #             num_hits=connector_prefix_cache_hits,
+                #             preempted=request.num_preemptions > 0,
+                #         )
+
+                # Request was already popped from self.waiting
+                # unless it was re-added above due to new_blocks being None.
+                request = self.waiting.pop_request()
+
+                self.running.append(request)
+                if self.log_stats:
+                    request.record_event(
+                        EngineCoreEventType.SCHEDULED, scheduled_timestamp
+                    )
+                if request.status == RequestStatus.WAITING:
+                    scheduled_new_reqs.append(request)
+                elif request.status == RequestStatus.PREEMPTED:
+                    scheduled_resumed_reqs.append(request)
+                else:
+                    raise RuntimeError(f"Invalid request status: {request.status}")
+
+                num_scheduled_tokens[request_id] = num_new_tokens
+                token_budget -= num_new_tokens
+                request.status = RequestStatus.RUNNING
+                request.num_computed_tokens = num_computed_tokens
+                # Count the number of prefix cached tokens.
+                if request.num_cached_tokens < 0:
+                    request.num_cached_tokens = num_computed_tokens
+
+        # Put back any skipped requests at the head of the waiting queue
+        if skipped_waiting_requests:
+            self.waiting.prepend_requests(skipped_waiting_requests)
+
+        # Check if the scheduling constraints are satisfied.
+        total_num_scheduled_tokens = sum(num_scheduled_tokens.values())
+        assert total_num_scheduled_tokens <= self.max_num_scheduled_tokens
+        assert token_budget >= 0
+        assert len(self.running) <= self.max_num_running_reqs
+
+        # Since some requests in the RUNNING queue may not be scheduled in
+        # this step, the total number of scheduled requests can be smaller than
+        # len(self.running).
+        assert len(scheduled_new_reqs) + len(scheduled_resumed_reqs) + len(
+            scheduled_running_reqs
+        ) <= len(self.running)
+
+        # Get the longest common prefix among all requests in the running queue.
+        # This can be potentially used for cascade attention.
+        num_common_prefix_blocks = [0] * len(self.kv_cache_config.kv_cache_groups)
+        with record_function_or_nullcontext("schedule: get_num_common_prefix_blocks"):
+            if self.running:
+                any_request_id = self.running[0].request_id
+                num_common_prefix_blocks = (
+                    self.kv_cache_manager.get_num_common_prefix_blocks(any_request_id)
+                )
+
+        # Construct the scheduler output.
+        if self.use_v2_model_runner:
+            scheduled_new_reqs = scheduled_new_reqs + scheduled_resumed_reqs
+            scheduled_resumed_reqs = []
+            new_reqs_data = [
+                NewRequestData.from_request(
+                    req,
+                    req_to_new_blocks[req.request_id].get_block_ids(),
+                    req._all_token_ids,
+                )
+                for req in scheduled_new_reqs
+            ]
+        else:
+            new_reqs_data = [
+                NewRequestData.from_request(
+                    req, req_to_new_blocks[req.request_id].get_block_ids()
+                )
+                for req in scheduled_new_reqs
+            ]
+
+        with record_function_or_nullcontext("schedule: make_cached_request_data"):
+            cached_reqs_data = self._make_cached_request_data(
+                scheduled_running_reqs,
+                scheduled_resumed_reqs,
+                num_scheduled_tokens,
+                scheduled_spec_decode_tokens,
+                req_to_new_blocks,
+            )
+
+        # Record the request ids that were scheduled in this step.
+        self.prev_step_scheduled_req_ids.clear()
+        self.prev_step_scheduled_req_ids.update(num_scheduled_tokens.keys())
+
+        scheduler_output = SchedulerOutput(
+            scheduled_new_reqs=new_reqs_data,
+            scheduled_cached_reqs=cached_reqs_data,
+            num_scheduled_tokens=num_scheduled_tokens,
+            total_num_scheduled_tokens=total_num_scheduled_tokens,
+            scheduled_spec_decode_tokens=scheduled_spec_decode_tokens,
+            scheduled_encoder_inputs=scheduled_encoder_inputs,
+            num_common_prefix_blocks=num_common_prefix_blocks,
+            preempted_req_ids={req.request_id for req in preempted_reqs},
+            # finished_req_ids is an existing state in the scheduler,
+            # instead of being newly scheduled in this step.
+            # It contains the request IDs that are finished in between
+            # the previous and the current steps.
+            finished_req_ids=self.finished_req_ids,
+            free_encoder_mm_hashes=self.encoder_cache_manager.get_freed_mm_hashes(),
+        )
+
+        # # NOTE(Kuntai): this function is designed for multiple purposes:
+        # # 1. Plan the KV cache store
+        # # 2. Wrap up all the KV cache load / save ops into an opaque object
+        # # 3. Clear the internal states of the connector
+        # if self.connector is not None:
+        #     meta: KVConnectorMetadata = self.connector.build_connector_meta(
+        #         scheduler_output
+        #     )
+        #     scheduler_output.kv_connector_metadata = meta
+
+        # # Build the connector meta for ECConnector
+        # if self.ec_connector is not None:
+        #     ec_meta: ECConnectorMetadata = self.ec_connector.build_connector_meta(
+        #         scheduler_output
+        #     )
+        #     scheduler_output.ec_connector_metadata = ec_meta
+
+        with record_function_or_nullcontext("schedule: update_after_schedule"):
+            self._update_after_schedule(scheduler_output)
+        return scheduler_output
---
+++ schedulers-v2/sjf.py
@@ -0,0 +1,217 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
+import enum
+
+# import os
+# import random
+# import time
+# from collections import deque
+# from dataclasses import dataclass, field
+from typing import Callable, Optional
+from random import seed, randint, uniform
+import joblib
+import numpy as np
+
+from vllm.config import CacheConfig, LoRAConfig, SchedulerConfig
+from vllm.sequence import SequenceGroup
+
+from prioritySchedBase import Scheduler
+
+
+class OracleFields(enum.Enum):
+    KEY = 0
+    PROMPT = 1
+    GENERATE = 2
+
+
+KEY_TOKEN_IDX = 3
+
+
+class EEVDF(Scheduler):
+    """docstring for EEVDF"""
+
+    def __init__(
+        self,
+        scheduler_config: SchedulerConfig,
+        cache_config: CacheConfig,
+        lora_config: Optional[LoRAConfig],
+        pipeline_parallel_size: int = 1,
+        output_proc_callback: Optional[Callable] = None,
+    ) -> None:
+        super(EEVDF, self).__init__(
+            scheduler_config,
+            cache_config,
+            lora_config,
+            pipeline_parallel_size,
+            output_proc_callback,
+        )
+
+        # Oracle stuff to test if predicting the len would result in SRTF
+        self.using_oracle = True
+        noise = 0
+        max_expected_time = 1000
+        min_expected_time = 1
+        seed(0)  # to make it reproducible
+        self.oracle = dict()
+        #with open("oracle_costs_sharegpt200.txt", "r") as file:
+        #with open(f"/sonic_home/willianjunior/vllm-segment/git/vllm-sched/studies/3-goodput-many-scheds/oracle.txt", "r") as file:
+        with open(f"0-sjf-oracle-logs/oracle.log", "r") as file:
+            next(file) # skip first line of columns name
+            for line in file:
+                parts = line.strip().split()
+                assert len(parts) >= 3, "malformed lines"
+                key = int(parts[OracleFields.KEY.value])
+                value1 = int(parts[OracleFields.PROMPT.value])
+                value2 = int(parts[OracleFields.GENERATE.value])
+
+                # Add some noise
+                # this is relative noise, does little harm
+                # noise_val = value2 * uniform(-noise, noise)
+                # print(f"{key} original {value2} noise: {noise_val}")
+                # value2 += noise_val
+                # value2 = max(1, value2)  # avoid negative numbers
+
+                # now some absolute noise
+                noise_val = uniform(-noise, noise) * randint(
+                    min_expected_time, max_expected_time
+                )
+
+                # bad :( except for small max_expected_time (400 is ok)
+                # noise_val = choice([-1, 1]) * randint(
+                #    min_expected_time, max_expected_time
+                # )
+
+                print(f"{key} original {value2} noise: {noise_val}")
+                value2 += noise_val
+                value2 = max(1, value2)  # avoid negative numbers
+
+                # just use a random number
+                # value2 = randint(min_expected_time, max_expected_time)
+
+                self.oracle[key] = [key, value1, value2]
+
+        # Load estimator model
+        #self.estimator = joblib.load(
+        #    "/sonic_home/willianjunior/vllm-segment/git/vllm-sched/llm-len-regression/models/random-forest-model-335.pkl.qrf"
+        #)
+
+        # Just a different random with extra steps
+        # Also didn't work ...
+        # data = [gen_val for _, _, gen_val in self.oracle.values()]
+        # kde = gaussian_kde(data)
+        # for key in self.oracle.keys():
+        #    self.oracle[key][OracleFields.GENERATE.value] = max(int(kde.resample(1)), 1)
+
+        # === EEVDF stuff... ==================================================
+        # [Will]: Monkey patching SequenceGroup to have virtual runtimes.
+        # expected_time_slice represents how much time is required to process
+        # the sequence. This value can change with the progress of the
+        # execution.
+        self._base_expected_time_slice = 10
+        if self.using_oracle:
+            self._base_expected_time_slice = -1
+
+        setattr(SequenceGroup, "total_vtime", 0)
+        setattr(SequenceGroup, "cur_vtime", 0)
+        setattr(SequenceGroup, "expected_time_slice", self._base_expected_time_slice)
+        setattr(SequenceGroup, "priority", 1)
+        setattr(SequenceGroup, "lag", 0)
+        setattr(SequenceGroup, "vdeadline", 0)
+
+        setattr(SequenceGroup, "slice_increment", 10)
+
+        # Number of tokens (or time spent) per sched step by each seq
+        # If using multi-step scheduling, it would be more than 1
+        # per max_num_seqs.
+        self.sched_slice = 1
+        self.max_num_seqs = self.scheduler_config.max_num_seqs
+
+    def _get_oracle(self, seq_group):
+        #key = seq_group.first_seq.inputs["prompt_token_ids"][KEY_TOKEN_IDX]
+        prompt_tokens = seq_group.first_seq.inputs["prompt_token_ids"]
+        # Token 60 is ']' and token[3] is '[', concat all between
+        key = "".join(map(str, prompt_tokens[3:prompt_tokens.index(60)]))
+        return self.oracle[int(key)][OracleFields.GENERATE.value]
+
+    def _update_queue_size(self, n):
+        self.queue_size = n
+
+        if self.queue_size < self.max_num_seqs:
+            self.ideal_slice = 1
+        else:
+            self.ideal_slice = (self.max_num_seqs * self.sched_slice) / self.queue_size
+
+    def _update_finished_priority(self, seq_group):
+        pass
+
+    def _update_running_priority(self, seq_group):
+        if (
+            seq_group.expected_time_slice == self._base_expected_time_slice
+            and self.using_oracle
+        ):
+            seq_group.expected_time_slice = self._get_oracle(seq_group)
+
+        seq_group.cur_vtime += self.sched_slice
+
+        # lag is how much time it used (sched_slice) minus how much
+        # time it was owed (ideal_slice). reducing lag means running
+        seq_group.lag -= self.sched_slice - self.ideal_slice
+        if seq_group.lag < 0:
+            seq_group.vdeadline = float("inf")
+        else:
+            seq_group.vdeadline = seq_group.lag + seq_group.expected_time_slice
+        seq_group.total_vtime += self.sched_slice
+
+    def _update_waiting_priority(self, seq_group):
+        if (
+            seq_group.expected_time_slice == self._base_expected_time_slice
+            and self.using_oracle
+        ):
+            seq_group.expected_time_slice = self._get_oracle(seq_group)
+
+        # didn't run last time, thus accumulating lag
+        seq_group.lag += self.ideal_slice
+        if seq_group.lag < 0:
+            seq_group.vdeadline = float("inf")
+        else:
+            seq_group.vdeadline = seq_group.lag + seq_group.expected_time_slice
+
+    def _priosched_should_update_waiting_1(self):
+        return True
+
+    def _can_preempt(self, seq_group):
+        # preemption condition
+        # return seq_group.cur_vtime >= self.min_vtime_run
+        # return True
+        return seq_group.cur_vtime > seq_group.expected_time_slice * 1
+
+    def _should_preempt(self, victim, sub):
+        # print(f"--------testing {self.print_seq(victim)} -> {self.print_seq(sub)}")
+        # if self._can_preempt(victim):
+        #    print("    can preempt")
+        return self._can_preempt(victim) and sub.vdeadline < victim.vdeadline
+
+    def _added_sequence_to_running(self, seq_group):
+        # just executed some tokens and still didn't finished
+        # if seq_group.total_vtime >= seq_group.expected_time_slice:
+        # Initial heuristic: double the expected time
+        # if the current time was not enough.
+        # TODO: try fibonacci?
+
+        # Expected time wasn't enough, add more time
+        if seq_group.expected_time_slice <= seq_group.total_vtime:
+            # seq_group.expected_time_slice *= 2
+            seq_group.expected_time_slice += seq_group.slice_increment
+            seq_group.slice_increment *= 4
+        seq_group.cur_vtime = 0
+        # seq_group.num_steps += 1
+
+    def priority(self, seq_group):
+        return seq_group.vdeadline
+
+    def print_seq(self, seq_group):
+        return (
+            f"lag={seq_group.lag:.2f} - vdeadline={seq_group.vdeadline:.2f} "
+            f"- {seq_group.cur_vtime}/{seq_group.total_vtime}/{seq_group.expected_time_slice}"
+        )
